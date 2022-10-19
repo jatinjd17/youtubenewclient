6bc1bcb86114a9731b5bcdc07927b0f361dda4fd
jatinjd17 <jatindharmik@gmail.com>
2022-10-20 03:46:47 +0530

    new server address added2

--- next.config.js
+++ next.config.js
@@ -1,41 +1,7 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-//   swcMinify: true,
-// }
-
-// module.exports = nextConfig
-
-module.exports = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; font-src 'self' 'http://52.66.214.219:3000'; img-src 'self' *.somewhere.com; script-src 'self'",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "DENY",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
-          },
-          {
-            key: "Permissions-Policy",
-            value:
-              "camera=(); battery=(self); geolocation=(); microphone=('https://somewhere.com')",
-          },
-        ],
-      },
-    ];
-  },
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
 };
+
+module.exports = nextConfig;
